99356037a490a0d9be79b958437b7054ce41c2ee
Abhradeep De <141354213+abhradeepde123@users.noreply.github.com>
2024-01-22 16:51:52 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ import os
 if __name__ == "__main__":
   print("Installing dependencies...")
   os.system("pip install -r requirements.txt")
+
+  print("Do 'python main.py' to run the bot")
   
   print("Deleting myself...")
   os.remove(__file__)
